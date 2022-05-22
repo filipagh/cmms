e555eb41a001b7c78e03f8d42186d06e13d3ddf1
Filip Agh <filip11agh@gmail.com>
2022-05-23 01:36:47 +0200

    hack for named routes

--- server/server.py
+++ server/server.py
@@ -4,25 +4,26 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 class xxxxx(SimpleHTTPRequestHandler):
 
     def do_GET(self):
+        path = self.translate_path(self.path)
+        f = None
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.path = '/'
+            ff = self.send_head()
+            self.copyfile(ff, self.wfile)
+            ff.close()
+            return
+        finally:
+            if f:
+                f.close()
+
         f = self.send_head()
         if f:
             try:
                 self.copyfile(f, self.wfile)
+            finally:
                 f.close()
-                return
-            except:
-                self.path = '/'
-                f = self.send_head()
-                self.copyfile(f, self.wfile)
-                f.close()
-                return
-
-
-
-        self.path = '/'
-        f = self.send_head()
-        self.copyfile(f, self.wfile)
-        f.close()
 
 
 def run(server_class=HTTPServer, handler_class=xxxxx):
