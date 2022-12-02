459d27f41193e33631d6de5de8e0b1d339f140ff
fagh <filip11agh@gmail.com>
2022-12-02 02:54:42 +0100

    api gen

--- open_cmms/api/lib/api/task_manager_api.dart
+++ open_cmms/api/lib/api/task_manager_api.dart
@@ -69,6 +69,59 @@ class TaskManagerApi {
     return null;
   }
 
+  /// Cancel Task
+  ///
+  /// Note: This method returns the HTTP [Response].
+  ///
+  /// Parameters:
+  ///
+  /// * [String] taskId (required):
+  Future<Response> cancelTaskTaskManagerTaskIdDeleteWithHttpInfo(String taskId,) async {
+    // ignore: prefer_const_declarations
+    final path = r'/task-manager/{task_id}'
+      .replaceAll('{task_id}', taskId);
+
+    // ignore: prefer_final_locals
+    Object? postBody;
+
+    final queryParams = <QueryParam>[];
+    final headerParams = <String, String>{};
+    final formParams = <String, String>{};
+
+    const contentTypes = <String>[];
+
+
+    return apiClient.invokeAPI(
+      path,
+      'DELETE',
+      queryParams,
+      postBody,
+      headerParams,
+      formParams,
+      contentTypes.isEmpty ? null : contentTypes.first,
+    );
+  }
+
+  /// Cancel Task
+  ///
+  /// Parameters:
+  ///
+  /// * [String] taskId (required):
+  Future<String?> cancelTaskTaskManagerTaskIdDelete(String taskId,) async {
+    final response = await cancelTaskTaskManagerTaskIdDeleteWithHttpInfo(taskId,);
+    if (response.statusCode >= HttpStatus.badRequest) {
+      throw ApiException(response.statusCode, await _decodeBodyBytes(response));
+    }
+    // When a remote server returns no body with a status of 204, we shall not decode it.
+    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
+    // FormatException when trying to decode an empty string.
+    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
+      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'String',) as String;
+    
+    }
+    return null;
+  }
+
   /// Create Component Task
   ///
   /// Note: This method returns the HTTP [Response].
