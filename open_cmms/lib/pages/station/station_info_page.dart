b7cbc1ceea983abdf2490a8c2612cf55ead1f4a7
fagh <filip11agh@gmail.com>
2023-02-23 02:36:13 +0100

    FE station info

--- open_cmms/lib/pages/station/station_info_page.dart
+++ open_cmms/lib/pages/station/station_info_page.dart
@@ -2,21 +2,29 @@ import 'package:BackendAPI/api.dart';
 import 'package:flutter/material.dart';
 import 'package:open_cmms/pages/station/station_base_page.dart';
 
-
-
-class StationInfoPage extends StatelessWidget implements StationBaseContextPage {
+class StationInfoPage extends StatelessWidget
+    implements StationBaseContextPage {
   static const String ENDPOINT = '/Info';
   final StationSchema station;
+
   const StationInfoPage({Key? key, required this.station}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("Meno: " + station.name),
+        SelectableText("Meno stanice: " + station.name),
+        SelectableText(
+            "Km cestneho useku: " + (station.kmOfRoad?.toString() ?? "")),
+        SelectableText("Km cestneho useku poznamka: " + station.kmOfRoadNote),
+        SelectableText(
+            "Gps - dlzka: " + (station.longitude?.toString() ?? "-")),
+        SelectableText("Gps - sirka: " + (station.latitude?.toString() ?? '-')),
+        SelectableText(
+            "nadmorska vyska: " + (station.seeLevel?.toString() ?? '-')),
+        SelectableText("poznamka: " + station.description),
         SelectableText("ID: " + station.id),
       ],
     );
   }
-
 }
