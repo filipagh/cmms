ee73360b86f1653880c514aac6b3090706a2fc8b
fagh <filip11agh@gmail.com>
2023-03-04 22:15:48 +0100

    be set BE path

--- backend/run.sh
+++ backend/run.sh
@@ -2,4 +2,4 @@
 pwd
 cd /backend
 echo $BE_PORT
-uvicorn base.main:app --proxy-headers --host 0.0.0.0 --port $BE_PORT --root-path /
+uvicorn base.main:app --proxy-headers --host 0.0.0.0 --port $BE_PORT --root-path $BE_PATH
