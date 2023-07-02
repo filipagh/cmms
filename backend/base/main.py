50fe93d6bf09db52d62e05567a1d7661f7305f47
Filip Agh <filip11agh@gmail.com>
2023-07-02 21:58:35 +0200

    test

--- backend/base/main.py
+++ backend/base/main.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import time
 from enum import Enum
 from urllib.request import Request
@@ -66,12 +67,18 @@ def add_transconder(t: Transcoding):
                 x.transcoder.register(t)
 
 
+proc = subprocess.Popen(["alembic upgrade head"], stdout=subprocess.PIPE, shell=True)
+(out, err) = proc.communicate()
+print("program output:", out)
+print("program error:", err)
+
 os.chdir(os.path.dirname(__file__) + '/../')
 if os.system('alembic upgrade head') != 0:
     print("ALEMBIC FAIL")
     # temporally solution until proper loging is done (sleep -> ability to see logs in docker logs)
     while True:
         time.sleep(10)
+        print("ALEMBIC FAIL")
 
 
 class Services(Enum):
